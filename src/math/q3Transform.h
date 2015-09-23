d5e2c274012a9085e825b71847d893c4f21868f5
Gabriel Chan <ycchangabriel@gmail.com>
2015-09-23 17:49:02 +0800

    Added q3Transform constructor

--- src/math/q3Transform.h
+++ src/math/q3Transform.h
@@ -37,6 +37,11 @@ struct q3Transform
 {
 	q3Vec3 position;
 	q3Mat3 rotation;
+	q3Transform(q3Vec3 vec3, q3Mat3 mat3)
+	{
+		position = vec3;
+		rotation = mat3;
+	}
 };
 
 #include "q3Transform.inl"
